02688e16f71f63f46bd997a32b9c1f9f854e9224
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-03-07 17:01:23 +0000

    TenziesList

---
+++ tenzies/src/Components/TenziesCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TenziesCard = () => {
+  return (
+    <div>TenziesCard</div>
+  )
+}
---
+++ tenzies/src/Components/TenziesList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+export const TenziesList = () => {
+  const [numberList, setNumberList] = useState([
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+  ]);
+
+  console.log(numberList);
+  return <div>TenziesList</div>;
+};
